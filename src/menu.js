a2c4e3382326c09c926ea9843e5a9a28873dba65
thabo <thabochambule1@gmail.com>
2024-09-09 17:40:24 +0200

    add content to menu module

---
+++ src/menu.js
@@ -0,0 +1,10 @@
+function Menu (){
+    const content = document.createElement('div');
+    
+    const heading = document.createElement('h1')
+    heading.innerHTML = `Menu`
+    const heading2 = document.createElement('h1')
+    heading2.innerHTML = `Beverages`
+    const meals = document.createElement('')
+
+}
